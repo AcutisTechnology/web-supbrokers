f17397910eeea89f23f73d15853b6f411d6a675d
sergio.costa <sergio.filho@smartspace.us>
2026-05-07 21:22:03 -0300

    feat: add .env.local to .gitignore

--- src/shared/configs/api.ts
+++ src/shared/configs/api.ts
@@ -2,8 +2,7 @@ import ky from "ky";
 import { parseCookies } from "nookies";
 
 export const api = ky.create({
-  prefixUrl: `http://localhost:8000/api/v1/`,
-  // prefixUrl: `https://coral-app-yi7ul.ondigitalocean.app/api/v1/`,
+  prefixUrl: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/`,
   hooks: {
     beforeRequest: [
       (request) => {
